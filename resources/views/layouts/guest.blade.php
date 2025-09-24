4bdf2905dde8849ad6c80565e986cf50e6b02351
Davidgultom <DavidgultomM@gmail.com>
2025-09-24 14:14:56 +0700

    Memperbaiki Sedikit Bug Pada Halaman Login -> David

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,7 @@
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen bg-white">
             <a href="https://www.cbn.id/en/cbn-fiber"
                 class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                <img class="h-15 w-15" src="{{ asset('storage/assets/iconunpri-removebg-preview.png') }}" alt="Your Company">
+                <img class="h-32 w-32" src="{{ asset('storage/assets/royal_klinik.png') }}" alt="Your Company">
             </a>
             <div
                 class="w-full bg-white rounded-lg ring-2 inset-ring-2 inset-ring-gray-500 shadow-lg dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -58,9 +58,9 @@
                     <form class="space-y-4 md:space-y-6" action="{{ route('proses.login') }}" method="POST">
                         @csrf
                         <div class="relative z-0 my-8">
-                            <input type="text" name="username" autocomplete="off"
+                            <input type="email" name="email" autocomplete="off"
                                 class="block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                placeholder=" " value="{{ old('username') }}" />
+                                placeholder=" " value="{{ old('email') }}" />
                             <label for="text"
                                 class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto text-md">Username
                             </label>
